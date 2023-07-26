4029926ad372a6d9174211718eb32434f10aad47
ArielParicagua <ArielFParicaguaV@gmail.com>
2023-07-26 16:50:21 -0400

    Creacion de la base de datos para pruebas y tmabien de crear el servicio HeroesService para traer información de los héroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
+
 
 
 @NgModule({
@@ -15,6 +17,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
+    HttpClientModule,
     SharedModule,
   ],
   providers: [],
